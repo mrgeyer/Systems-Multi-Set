b9439bf40751d9bc95a9656f4a1f2619e840b081
Scottt <scott.geyer@eagles.usm.edu>
2017-04-13 21:24:27 -0500

    Update and rename loadStuff.js to saveStuff.js

--- script/saveStuff.js
+++ script/saveStuff.js
@@ -95,3 +95,16 @@ function destroyClickedElement(event)
 {
 	document.body.removeChild(event.target);
 }
+ 
+ function loadFileAsText()
+{
+    var fileToLoad = document.getElementById("fileToLoad").files[0];
+ 
+    var fileReader = new FileReader();
+    fileReader.onload = function(fileLoadedEvent) 
+    {
+        var textFromFileLoaded = fileLoadedEvent.target.result;
+        document.getElementById("inputTextToSave").value = textFromFileLoaded;
+    };
+    fileReader.readAsText(fileToLoad, "UTF-8");
+}
