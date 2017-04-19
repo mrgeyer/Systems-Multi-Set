c0907d083e388caea403b3bf06223ccffbefee04
Scottt <scott.geyer@eagles.usm.edu>
2017-04-19 14:07:50 -0500

    Update saveStuff.js

--- script/saveStuff.js
+++ script/saveStuff.js
@@ -1,6 +1,17 @@
 var questions = "";
 var worksheet = [];
 
+function pad(pad, str, padLeft) {
+  if (typeof str === 'undefined') 
+    return pad;
+  if (padLeft) {
+    return (pad + str).slice(-pad.length);
+  } else {
+    return (str + pad).substring(0, pad.length);
+  }
+}
+
+
 function saveTextAsFile() {
 	d = document.getElementById("setN").value;
 	dv = d;
@@ -9,6 +20,19 @@ function saveTextAsFile() {
 	var maxCoeff = document.getElementById("coeff").value;
 	var maxAns = document.getElementById("cons").value;
 	var sigDigs = document.getElementById("sigDig").value;
+	qPad = "&#160;&#160;&#160;&#160;&#160;"
+	coefPad = "&#160;&#160;"; // negative sign and decimal
+	conPad = "&#160;&#160;"; // negative sign and decimal
+	for (i = 1; i<maxCoeff*10; i*10) { // digits before decimal
+		coefPad += "&#160;";
+	}
+	for (i = 1; i<maxCoeff*maxAns; i*10) { // digits before decimal
+		conPad += "&#160;";
+	}
+	for (i = 1; i<sigDigs; i++) { // digits after decimal
+		coefPad += "&#160;";
+		conPad += "&#160;";
+	}
  	for (var i = 0; i < nQ; i++) {
 		a1 = Math.round(Math.random()*maxCoeff* Math.pow(10,sigDigs))/ Math.pow(10,sigDigs);
 		b1 =  Math.round(Math.random()*maxCoeff* Math.pow(10,sigDigs))/ Math.pow(10,sigDigs);
@@ -16,6 +40,7 @@ function saveTextAsFile() {
 		b2 =  Math.round(Math.random()*maxCoeff* Math.pow(10,sigDigs))/ Math.pow(10,sigDigs);	
 		x =   Math.round(Math.random()*maxAns* Math.pow(10,sigDigs))/ Math.pow(10,sigDigs);
 		y =   Math.round(Math.random()*maxAns* Math.pow(10,sigDigs))/ Math.pow(10,sigDigs);
+
 		if (Math.random() > .5) {
 			a1 = 0-a1;
 		}
@@ -42,12 +67,19 @@ function saveTextAsFile() {
 			sign2 = '-';
 			c2 = Math.round((a2*x - b2*y)*Math.pow(10,sigDigs*2))/ Math.pow(10,sigDigs*2);
 		}
-		questions += (i+1) + ") " + a1.toString() + "x " + sign1 + " " + b1.toString() + "y = " + c1.toString();
-		questions += "<br>&#160;&#160;&#160;&#160;" + a2.toString() + "x" + " " + sign2 + " " + b2.toString() + "y = " + c2.toString()+ '\n';
+		var Qn = pad(Qpad, (i+1) + ") ", true);
+		var a1s = pad(coefPad, a1, true);
+		var a2s = pad(coefPad, a2, true);
+		var b1s = pad(coefPad, b1, true);
+		var b2s = pad(coefPad, b2, true);
+		var c1s = pad(conPad, c1, true);
+		var c2s = pad(conPad, c2, true);
+		questions += Qn + a1s + "x " + sign1 + " " + b1s + "y = " + c1s;
+		questions += "<br>" + qPad + a2s + "x" + " " + sign2 + " " + b2s + "y = " + c2s + '\n';
 		//answers += a1.toString() + "x " + sign1 + " " + b1.toString() + "y = " + c1.toString();
 		//answers += "\n" + a2.toString() + "x " + sign2 + " " + b2.toString() + "y = " + c2.toString() + '\n';
 		answers += (i+1) + ". (" + x.toString() + ", " + y.toString() + ")\n\n";
-		wsi = [a1,b1,c1,sign1,a2,b2,c2,sign2];
+		wsi = [a1s,b1s,c1s,sign1,a2s,b2s,c2s,sign2];
 		worksheet.push(wsi);
 	}
 
@@ -87,10 +119,12 @@ function saveAnswersAsFile() {
 function saveWorksheetAsFile() {	
 	var textToSave = "System of Linear Equations Worksheet " + d + " \n\n";
 	for (i = 0; i < worksheet.length; i += 2) {
-		textToSave += (i+1) + ") " + worksheet[i][0] + "x " + worksheet[i][3] + " " + worksheet[i][1] + "y = " + worksheet[i][2];
-		textToSave += "         " + (i+2) + ") " + worksheet[i+1][0] + "x " + worksheet[i+1][3] + " " + worksheet[i+1][1] + "y = " + worksheet[i+1][2];
-		textToSave += "\n    " + worksheet[i][4] + "x " + worksheet[i][7] + " " + worksheet[i][5] + "y = " + worksheet[i][6];
-		textToSave += "             " + worksheet[i+1][4] + "x " + worksheet[i+1][7] + " " + worksheet[i+1][5] + "y = " + worksheet[i+1][6] + "\n\n";		
+		var Qn1 = pad(Qpad, (i+1) + ") ", true);
+		var Qn2 = pad(Qpad, (i+2) + ") ", true);
+		textToSave += Qn1 + worksheet[i][0] + "x " + worksheet[i][3] + " " + worksheet[i][1] + "y = " + worksheet[i][2];
+		textToSave += "         " + Qn2 + worksheet[i+1][0] + "x " + worksheet[i+1][3] + " " + worksheet[i+1][1] + "y = " + worksheet[i+1][2];
+		textToSave += "\n" + Qpad + worksheet[i][4] + "x " + worksheet[i][7] + " " + worksheet[i][5] + "y = " + worksheet[i][6];
+		textToSave += "         " + Qpad + worksheet[i+1][4] + "x " + worksheet[i+1][7] + " " + worksheet[i+1][5] + "y = " + worksheet[i+1][6] + "\n\n";		
 	}
 	var fileNameToSaveAs = "SysLinEqWS" + dv + ".txt";
 	var textToSaveAsBlob = new Blob([textToSave], {type:"text/plain"});
